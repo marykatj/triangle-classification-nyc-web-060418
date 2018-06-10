cde6a18a3b3e86e8304b7c410a8c8a848547c0ce
Mary-Kate James <marykatj@gmail.com>
2018-06-10 22:36:31 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+attr_accessor :length1, :length2, :length3
+
+def initialize(length1, length2, length3)
+  @length1 = length1
+  @length2 = length2
+  @length3 = length3
+end
+
+def kind
+  check_triangle
+  if @length1 == @length2 && @length1 == @length3
+    :equilateral
+  elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3
+    :isosceles
+  else :scalene
+  end
+end
+
+def check_triangle
+  real_triangle = [(@length1 + @length2 > @length3), (@length1 + @length3 > @length2), (@length3 + @length2 > @length1)]
+  [@length1, @length2, @length3].each { |s| real_triangle << false if s <= 0 }
+  raise TriangleError if real_triangle.include?(false)
+end
+
+class TriangleError < StandardError
+end
+
 end
