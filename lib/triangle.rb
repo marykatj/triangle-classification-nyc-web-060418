a44138b068c29f16000413148f894f1baeecd6c3
Mary-Kate James <marykatj@gmail.com>
2018-06-10 22:23:02 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ def kind
 
   elsif @length1 == @length2 && @length1 == @length3
     return :equilateral
-  elsif @length1 == @length2 || @length1 == @length3 || @length 2 == @length3
+  elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3
     return :isosceles
   else return :scalene
   end
