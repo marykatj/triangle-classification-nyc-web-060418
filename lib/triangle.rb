4605c25516e7c03a9090d4a6b739e20505227af5
Mary-Kate James <marykatj@gmail.com>
2018-06-10 22:10:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,16 @@
 class Triangle
 
+attr_accessor :length1, :length2, :length3
+
 def initialize(length1, length2, length3)
   @length1 = length1
   @length2 = length2
   @length3 = length3
+end
+
+class OurError < StandardError
+  def message "error message"
+  end
 end
 
 end
