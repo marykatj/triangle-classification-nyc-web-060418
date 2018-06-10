b09f744f002799f2ac79eb84993b55e9987935a0
Mary-Kate James <marykatj@gmail.com>
2018-06-10 22:33:25 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,32 +1,29 @@
-require 'pry'
-
 class Triangle
-
-attr_accessor :length1, :length2, :length3
-
-def initialize(length1, length2, length3)
-  @length1 = length1
-  @length2 = length2
-  @length3 = length3
-end
-
-def kind
-  check_triangle
-  if @length1 == @length2 && @length1 == @length3
-    :equilateral
-  elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3
-    :isosceles
-  else :scalene
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
-end
 
-def check_triangle
-  if @length1 = 0 || @length2 = 0 || @length3 = 0 || (@length1 + @length2) > @length3 || (@length2 + @length3) > @length1 || (@length1 + @length3) > @length2
-    raise TriangleError
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
   end
-end
 
-class TriangleError < StandardError
-end
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
+  end
+
+  class TriangleError < StandardError
+  end
 
 end
