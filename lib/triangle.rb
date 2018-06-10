c91468dd42d8f7119314a7ba79811714760a66c9
Mary-Kate James <marykatj@gmail.com>
2018-06-10 22:22:39 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ def kind
   if @length1 = 0 || @length2 = 0 || @length3 = 0 || (@length1 + @length2) > @length3 || (@length2 + @length3) > @length1 || (@length1 + @length3) > @length2
     raise TriangleError
 
-  elsif @length1 == @length2 == @length3
+  elsif @length1 == @length2 && @length1 == @length3
     return :equilateral
   elsif @length1 == @length2 || @length1 == @length3 || @length 2 == @length3
     return :isosceles
