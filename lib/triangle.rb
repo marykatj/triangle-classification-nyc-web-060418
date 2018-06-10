ae3197b57d7ad2b305407fd67cc0879ac7962b4c
Mary-Kate James <marykatj@gmail.com>
2018-06-10 22:29:02 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,15 +9,18 @@ def initialize(length1, length2, length3)
 end
 
 def kind
-  if @length1 = 0 || @length2 = 0 || @length3 = 0 || (@length1 + @length2) > @length3 || (@length2 + @length3) > @length1 || (@length1 + @length3) > @length2
-    raise TriangleError
-
+  check_triangle
   elsif @length1 == @length2 && @length1 == @length3
     :equilateral
   elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3
     :isosceles
   else :scalene
   end
+end
+
+def check_triangle
+  if @length1 = 0 || @length2 = 0 || @length3 = 0 || (@length1 + @length2) > @length3 || (@length2 + @length3) > @length1 || (@length1 + @length3) > @length2
+    raise TriangleError
 end
 
 class TriangleError < StandardError
